598f5ce04d866761a4e5438311c2a7172462dcbc
Omar Dib <odib@c2r6s6.42beirut.com>
2025-03-13 17:09:51 +0200

    fixes

--- py03/ex04/ft_calculator.py
+++ py03/ex04/ft_calculator.py
@@ -1,25 +1,25 @@
 class calculator:
     '''A calculator class without instantiation'''
     @staticmethod
-    def dotproduct(V1: list[float], V2: list[float])-> None:
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
         '''Multiply two vectors'''
         res = 0
-        for i in range(0,len(V1)):
+        for i in range(0, len(V1)):
             res += V1[i] * V2[i]
         print(f'Dot product is : {res}')
 
     @staticmethod
-    def add_vec(V1: list[float], V2: list[float])-> None:
+    def add_vec(V1: list[float], V2: list[float]) -> None:
         '''Add two vectors'''
         res = []
-        for i in range(0,len(V1)):
+        for i in range(0, len(V1)):
             res.append(float(V1[i] + V2[i]))
         print(f'Add Vector is : {res}')
-        
+
     @staticmethod
-    def sous_vec(V1: list[float], V2: list[float])-> None:
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
         '''Substract two vectors'''
         res = []
-        for i in range(0,len(V1)):
+        for i in range(0, len(V1)):
             res.append(float(V1[i] - V2[i]))
         print(f'Sous Vector is : {res}')
